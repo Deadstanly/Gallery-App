0deca53fc096c881b970dd74b468e7790e1605ff
Vladislav_Kazakov <72248021+Deadstanly@users.noreply.github.com>
2022-04-03 18:24:46 +0300

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  authUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+  apiKey: 'AIzaSyAK3HEmcaTHuUwJ0eKr9DLMGVQY-xDI9Kk',
+  flickrUrl: 'https://www.flickr.com/services/rest/',
+  flickrKey: '262aa6e88d60554373367fb8efc7e818'
 };
